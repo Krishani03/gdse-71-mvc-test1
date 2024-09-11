24cb2c9a4de7983bd468968bac1f4f84cda46974
Krishani <krishanik932@gmail.com>
2024-09-11 11:08:07 +0530

    config DBConnection

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package customer;
+package controller;
 
 /**
  *
  * @author krishani
  */
-public class Controller {
+public class CustomerController {
     
 }
